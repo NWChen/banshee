a53b87b04248db6d76ffd20f19e511a624c77512
Neil Chen <neilwchen@gmail.com>
2017-11-13 16:53:18 -0500

    Convert tabs to 4-spaces

--- tests/test_twitter_requests.py
+++ tests/test_twitter_requests.py
@@ -1,22 +1,22 @@
 from banshee.scrapers.twitter_requests import Scrape
 
 def test_get_by_username():
-	"""
-	prints out the returned information
-	"""
-	scrape = Scrape()
-	statuses = scrape.get_by_username('realDonaldTrump')
-	print(statuses)
+    """
+    prints out the returned information
+    """
+    scrape = Scrape()
+    statuses = scrape.get_by_username('realDonaldTrump')
+    print(statuses)
 
 def test_get_by_keywords():
-	scrape = Scrape()
-	statuses = scrape.get_by_keywords(['donald', 'trump'])
-	print(statuses)
+    scrape = Scrape()
+    statuses = scrape.get_by_keywords(['donald', 'trump'])
+    print(statuses)
 
 def test_retweets():
-	scrape = Scrape()
-	statuses = scrape.get_retweets(929717762422988801)
-	print(statuses)
+    scrape = Scrape()
+    statuses = scrape.get_retweets(929717762422988801)
+    print(statuses)
 
 test_get_by_username()
 print('done')
