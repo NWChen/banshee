d3720f6b2b8d872daf8e8265c4af6aefef04bd25
Neil Chen <neilwchen@gmail.com>
2017-11-16 14:48:31 -0500

    Fix threading import

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify, render_template, request, Response
 from flask_socketio import SocketIO
 from scrapers.twitter_requests import Scrape
+from threading import Event, Thread
+
+import json
 import os
 
 app = Flask(__name__)
@@ -9,9 +12,37 @@ socketio = SocketIO(app)
 thread = Thread()
 thread_stop_event = Event()
 
+'''
+Scrape Twitter for recent data at regular intervals.
+'''
+class DataThread(Thread):
+    def __init__(self):
+        self.interval = 1.0
+        self.keywords = ''
+        super(DataThread, self).__init__()
+
+    def generate_data(self):
+        while not thread_stop_event.isSet():
+            tweets = scraper.get_by_keywords(self.keywords.split(','))
+            tweets = json.dumps(tweets)
+            print(tweets)
+            socketio.emit('data', {'contents': tweets}, namespace='/data')
+            sleep(self.interval)
+
+    def run(self):
+        self.generate_data()
+
+'''
+Handle client requests and serve relevant client views.
+'''
 @socketio.on('client_connected')
 def client_connected(data):
     print('client connected with message: ' + data)
+    global thread #TODO: make this not global
+    if not thread.isAlive():
+        print('STARTING THREAD')
+        thread = DataThread()
+        thread.start()
 
 @app.route('/')
 def index():
@@ -33,5 +64,6 @@ def index_post():
     return render_template('index.html', data=tweets)
 
 if __name__ == '__main__':
+    print('APP STARTED')
     socketio.run(app)
     #app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
