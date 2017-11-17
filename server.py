3b84829dbd52440e5e165cbe359a55c09e1fee6e
pr <peter@peter.net>
2017-11-16 20:56:11 -0500

    Initial Results Reformat
    This is now deploying on my machine.

--- server.py
+++ server.py
@@ -67,3 +67,4 @@ if __name__ == '__main__':
     print('APP STARTED')
     socketio.run(app)
     #app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    #For Peter: socketio.run(app, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
