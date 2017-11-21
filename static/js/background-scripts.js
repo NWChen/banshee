15bdc370e40307d713f7bde2e6bada74f1fa43e9
Neil Chen <neilwchen@gmail.com>
2017-11-21 15:27:13 -0500

    Add syntax formatting with span

--- static/js/background-scripts.js
+++ static/js/background-scripts.js
@@ -1,35 +1,49 @@
+const get_inputs = function() {
+    var inputs = {
+        username: $('#username').val(),
+        nearest_location: $('#location').val(),
+        mile_radius: $('#mile_radius').val(),
+        any_words: $('#any_words').val(),
+        all_words: $('#all_words').val(),
+        exact_phrase: $('#exact_phrase').val()
+    };
+    return inputs;
+};
+
+const send_inputs = function(socket, inputs) {
+    socket.emit('inputs', inputs);
+};
+
+const format_data = function(data, keyword) { // TODO - handle multiple keywords
+    data = JSON.parse(data);
+    var contents = data['content'];
+    var id = data['url'];
+    data['url'] = id.substring(id.lastIndexOf('/'))
+    var tag_start = '<span class=\"keyword\">'
+    var tag_end = '</span>';
+
+    contents = contents.replace(new RegExp(keyword, 'g'), tag_start + keyword + tag_end);
+    data['content'] = contents;
+    return data; // an Object with the same fields as the parameter data
+};
+
+const setup_listeners = function(socket) {
+    // Client event handlers
+    $('#search').click(function() {
+        send_inputs(socket, get_inputs()); 
+    });
+
+    // Socket event handlers
+    socket.on('data', function(data) {
+        tweet_html = format_data(data['data'], data['keywords']);
+        $('.content').html(tweet_html['content']);
+        //$('#data').html(data['data']);
+    });
+}
+
 $(document).ready(function() {
-
-    // these long function names are essentially documentation, and should never be called
-    const get_inputs = function getValuesOfAllInputs() {
-        var inputs = {
-            username: $('#username').val(),
-            earliest_date: $('#earliest-date').val(),
-            latest_date: $('#latest-date').val(),
-            keywords: $('#keywords').val(),
-            retweets: $('#retweets').val()
-        };
-        return inputs;
-    };
-
-    const send_inputs = function sendValuesOfAllInputsToServerSocket(socket, inputs) {
-        socket.emit('inputs', inputs);
-    };
-
     var socket = io.connect('http://' + document.domain + ':' + location.port + '/stream');
     socket.on('connect', function() {
-        console.log('CONNECTED');
-
-        // Client event handlers
-        $('#search').click(function() {
-            inputs = get_inputs();
-            send_inputs(socket, inputs); 
-        });
-
-        // Socket event handlers
-        socket.on('tweets', function(data) {
-            console.log(data);
-            $('#data').html(data['tweets']);
-        });
+        setup_listeners(socket);
     });
 });
