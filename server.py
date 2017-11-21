bd93b828c1e30e284397b1de849a7510557a93fc
Neil Chen <neilwchen@gmail.com>
2017-11-21 12:55:03 -0500

    Update server

--- server.py
+++ server.py
@@ -1,25 +1,33 @@
 from datetime import date, datetime
 from flask import Flask, jsonify, render_template, request, Response
 from flask_socketio import SocketIO
-from scrapers.twitter_requests import Scrape
+from scrapers.search_requests import Requests as scraper
 from time import sleep
 
 import json
 import os
 
 app = Flask(__name__)
-scraper = Scrape()
 socketio = SocketIO(app)
 
 '''
-Serialize JSON objects that include datetimes.
+Serialize JSON objects that include datetimes
 '''
-def json_serial(list_of_objs):
-    for objs in list_of_objs: #TODO: make this more efficient
+def serial_json(list_of_objs):
+    for objs in list_of_objs:
         for key in objs.keys():
             if isinstance(objs[key], (datetime, date)):
                 objs[key] = objs[key].isoformat()
     return objs
+
+'''
+Generate formatted JSON based on included tokens.
+#TODO - delete this?
+'''
+def format_json(tweet:dict, keyword=''):
+    data = {}
+    content = tweet['content']
+    return json.dumps(data)
 
 '''
 Confirm socket connection to the client.
@@ -33,10 +41,11 @@ Listen to and process inputs. Return outputs to the client.
 '''
 @socketio.on('inputs', namespace='/stream')
 def handle_inputs(data):
-    keywords = data['keywords'].split(' ')
-    tweets = scraper.get_by_keywords(keywords)
-    tweets = json.dumps(json_serial(tweets))
-    socketio.emit('tweets', {'tweets': tweets}, namespace='/stream')
+    print(data)
+    exact_phrase = data['exact_phrase'] #TODO: change fields to the new form inputs
+    tweets = scraper.search_exact_phrase(exact_phrase)
+    tweets = json.dumps(serial_json(tweets))
+    socketio.emit('data', {'data': tweets}, namespace='/stream')
 
 '''
 Serve homepage.
