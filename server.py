35a32921129b1ebe18419361dd52d78de85b53d7
Neil Chen <neilwchen@gmail.com>
2017-11-25 22:04:05 -0500

    No JSON datetime serialization necessary, since 'date' has type str

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from datetime import date, datetime
-from flask import Flask, jsonify, request, Response, send_file
+from flask import Flask, jsonify, request, Response, send_file, send_from_directory
 from flask_socketio import SocketIO
 from scrapers.search_requests import Requests as Scraper
 from time import sleep
@@ -7,19 +7,9 @@ from time import sleep
 import json
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 scraper = Scraper()
 socketio = SocketIO(app)
-
-'''
-Serialize JSON objects that include datetimes
-'''
-def serial_json(list_of_objs):
-    for objs in list_of_objs:
-        for key in objs.keys():
-            if isinstance(objs[key], (datetime, date)):
-                objs[key] = objs[key].isoformat()
-    return objs
 
 '''
 Generate formatted JSON based on included tokens.
@@ -48,34 +38,42 @@ def handle_inputs(data):
     exact_phrase = data['exact_phrase'] #TODO: change fields to the new form inputs
     tweets = []
     if exact_phrase:
-        tweets.extend(scraper.search_exact_phrase(exact_phrase))
+        data = scraper.search_exact_phrase(exact_phrase)
+        tweets.extend(data)
     if username:
-        temp = scraper.search_user(username) #TODO: clean this up
-        tweets.extend(temp)
+        data = scraper.search_user(username) #TODO: clean this up
+        print(data)
+        tweets.extend(data)
     # this can be the implementation of the caching
+    '''
     filtered = []
     for tweet in tweets:
         if tweet['url'] not in seen_tweets:
             filtered.append(tweet)
             seen_tweets.add(tweet['url'])
     tweets = filtered
+    '''
     # optional- to create two cache sets, one for last batch, one for current batch
     # then replace last = current at the end of the loop
     # this would be because no more than 40 tweets will arrive from each request
     # so no need to remember further back than the last batch or two
 
-    tweets = json.dumps(serial_json(tweets))
-
-    keywords = exact_phrase #TODO: adapt this for other fields too
-    print(tweets, keywords)
-    socketio.emit('data', {'data': tweets, 'keywords': keywords}, namespace='/stream')
+    socketio.emit('data', {'data': tweets}, namespace='/stream')
 
 '''
-Serve homepage.
+Serve homepage and related assets.
 '''
 @app.route('/')
-def index():
+def root():
     return send_file('index.html')
+
+@app.route('/js/<path:path>')
+def js(path):
+    return send_from_directory('js', path)
+
+@app.route('/css/<path:path>')
+def css(path):
+    return send_from_directory('css', path)
 
 if __name__ == '__main__':
     print('APP STARTED')
