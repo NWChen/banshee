945e27cae9d086ef674bdb54098c3af301d2dab7
Neil Chen <neilwchen@gmail.com>
2017-11-25 20:41:22 -0500

    Comment JS and move CDN links to local

--- static/js/background-scripts.js
+++ static/js/background-scripts.js
@@ -1,3 +1,7 @@
+/*
+ * Grabs input values from all inputs on the frontend,
+ * and arranges them in a JS object for future processing.
+ */
 const get_inputs = function() {
     var inputs = {
         username: $('#username').val(),
@@ -10,40 +14,57 @@ const get_inputs = function() {
     return inputs;
 };
 
+/*
+ * Whenever the user edits values in the input boxes on the frontend,
+ * those values are sent via WebSockets to the server for processing.
+ */
 const send_inputs = function(socket, inputs) {
     socket.emit('inputs', inputs);
 };
 
-const format_data = function(data, keyword) { // TODO - handle multiple keywords
-    data = JSON.parse(data);
-    var contents = data['content'];
-    var id = data['url'];
-    data['url'] = id.substring(id.lastIndexOf('/'))
-    var tag_start = '<span class=\"keyword\">'
-    var tag_end = '</span>';
-
-    contents = contents.replace(new RegExp(keyword, 'g'), tag_start + keyword + tag_end);
-    data['content'] = contents;
-    return data; // an Object with the same fields as the parameter data
+/*
+ * Update the tweet feed in `templates/index.html`.
+ * Uses Handlebars expressions to dynamically update content.
+ * The template for this content is built into `index.html`.
+ */
+const update_template = function(data, template) {
+    var context = {
+        'name': data['name'],
+        'text': data['text'],
+        'timestamp': data['timestamp'],
+        'id_str': data['id_str']
+    };
+    var compiled_html = template(context);
+    $('.content-placeholder').html(compiled_html);
 };
-
-const setup_listeners = function(socket) {
-    // Client event handlers
-    $('#search').click(function() {
-        send_inputs(socket, get_inputs()); 
-    });
-
-    // Socket event handlers
-    socket.on('data', function(data) {
-        tweet_html = format_data(data['data'], data['keywords']);
-        $('.content').html(tweet_html['content']);
-        //$('#data').html(data['data']);
-    });
-}
 
 $(document).ready(function() {
     var socket = io.connect('http://' + document.domain + ':' + location.port + '/stream');
+    var template = Handlebars.compile($('#address-template').html());
+
+    /*
+     * If WebSocket connection between client and server fails, we have a problem,
+     * and the app cannot function.
+     * TODO handle this case
+     */
     socket.on('connect', function() {
-        setup_listeners(socket);
+
+        /*
+         * Handle behaviors on clicking the 'SEARCH' button.
+         * TODO enable this behaviors when 'enter' is pressed as well.
+         * Sends values of all input boxes to the receiving WebSocket on the server.
+         */
+        $('#search').click(function() {
+            send_inputs(socket, get_inputs()); 
+        });
+
+        /*
+         * Handle behaviors upon receiving data from the server.
+         * This happens asynchronously, i.e. no signals for data are sent from the client to the server.
+         * TODO make this actually true via streaming data
+         */
+        socket.on('data', function(data) {
+            update_template(data, template);
+        });
     });
 });
